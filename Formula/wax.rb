4533769789c569c46c019a5ecbd3c35b59c3c578
christopherkarani <chrisbkarani@gmail.com>
2026-03-03 07:50:59 +0300

    wax: bump to waxmcp-v0.1.13

--- Formula/wax.rb
+++ Formula/wax.rb
@@ -1,15 +1,15 @@
 class Wax < Formula
   desc "CLI for the Wax MCP server"
   homepage "https://github.com/christopherkarani/Wax"
-  url "https://github.com/christopherkarani/Wax/archive/refs/tags/waxmcp-v0.1.11.tar.gz"
-  sha256 "cb543353e3ff84d5043e100ae02794ff33e3d6cadf3569b0ddf8269e8beac867"
+  url "https://github.com/christopherkarani/Wax/archive/refs/tags/waxmcp-v0.1.13.tar.gz"
+  sha256 "a055f14293a3314395d2d634dabf571e9e9a6127bda8899303ce8893fdb95c0f"
   license "MIT"
 
   depends_on xcode: ["15.0", :build]
 
   def install
-    system "swift", "build", "--disable-sandbox", "-c", "release", "--product", "WaxCLI"
-    bin.install ".build/release/WaxCLI" => "wax"
+    system "swift", "build", "--disable-sandbox", "-c", "release", "--product", "wax-cli"
+    bin.install ".build/release/wax-cli" => "wax"
   end
 
   test do
